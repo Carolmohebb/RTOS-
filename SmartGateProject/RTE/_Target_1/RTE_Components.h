860fa9dab7998baa5fc3df4463fee17e1c86976d
John <johnazizja10@gmail.com>
2026-04-16 20:46:22 +0200

    Add project configuration files for SmartGateProject
    - Created SmartGateProject.uvoptx for project options including target settings, debugging options, and listing configurations.
    - Created SmartGateProject.uvprojx for project structure, including target definitions, RTE components, and source file configurations.

---
+++ SmartGateProject/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,35 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SmartGateProject' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "TM4C123.h"
+
+/* ARM.FreeRTOS::RTOS:Config:FreeRTOS:11.2.0 */
+#define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
+/* ARM.FreeRTOS::RTOS:Core:Cortex-M:11.2.0 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/* ARM.FreeRTOS::RTOS:Heap:Heap_4:11.2.0 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/* ARM.FreeRTOS::RTOS:Timers:11.2.0 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/* Keil.ARM Compiler::Compiler:I/O:STDIN:Breakpoint:1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_BKPT      /* Compiler I/O: STDIN Breakpoint */
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:Breakpoint:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_BKPT     /* Compiler I/O: STDOUT Breakpoint */
+
+
+#endif /* RTE_COMPONENTS_H */
